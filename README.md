af3aae80f5d1b9f76ccb9c9071c4c85018a05d62
Samuel Walladge <samuel@swalladge.id.au>
2017-08-09 15:55:08 +0930

    start working on cli api

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # dotfiles-manager
 
+Copyright © 2017 Samuel Walladge
+
 dotfiles manager project for PRT455
+
+
+WIP
+
+```
+Dotfiles manager 0.0.1
+Samuel Walladge <samuel@swalladge.id.au>
+Manages dotfiles
+
+USAGE:
+    dotfiles-manager [FLAGS] [OPTIONS] [SUBCOMMAND]
+
+FLAGS:
+    -h, --help       Prints help information
+    -n, --no         Do not actually make any filesystem changes or run hooks
+    -V, --version    Prints version information
+    -v, --verbose    Be verbose
+
+OPTIONS:
+    -d, --dir <DIR>          Set dir to DIR (default is current dir)
+    -B, --hostname <NAME>    override computer's hostname to NAME
+    -t, --target <DIR>       Set target to DIR (default is $HOME)
+
+SUBCOMMANDS:
+    add        add a file to package
+    help       Prints this message or the help of the given subcommand(s)
+    install    install tags/packages
+    remove     remove tags/packages
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,125 @@
+extern crate clap;
+
 use std::path::{Path, PathBuf};
 use std::env;
+use clap::{Arg, App, SubCommand};
+use std::fs;
+
+use args::Args;
+
+mod args;
 
 fn main() {
+    let matches = App::new("Dotfiles manager")
+        .version("0.0.1")
+        .author("Samuel Walladge <samuel@swalladge.id.au>")
+        .about("Manages dotfiles")
+        .arg(
+            Arg::with_name("dir")
+                .short("d")
+                .long("dir")
+                .value_name("DIR")
+                .help("Set dir to DIR (default is current dir)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("hostname")
+                .short("B")
+                .long("hostname")
+                .value_name("NAME")
+                .help("override computer's hostname to NAME")
+                .takes_value(true),
+        )
+        // TODO: config file options
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .value_name("DIR")
+                .help("Set target to DIR (default is $HOME)")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("test").long("no").short("n").help(
+            "Do not actually make any filesystem changes or run hooks",
+        ))
+        .arg(Arg::with_name("verbose").long("verbose").short("v").help(
+            "Be verbose",
+        ))
+        .subcommand(
+            SubCommand::with_name("install")
+                .about("install tags/packages")
+                .arg(
+                    Arg::with_name("PACKAGE")
+                        .help("package name(s)")
+                        .required(true)
+                        .multiple(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("remove")
+                .about("remove tags/packages")
+                .arg(
+                    Arg::with_name("PACKAGE")
+                        .help("package name(s)")
+                        .required(true)
+                        .multiple(true),
+                )
+                .alias("uninstall"),
+        )
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("add a file to package")
+                .arg(
+                    Arg::with_name("file")
+                        .help("dotfile to add/adopt")
+                        .required(true),
+                )
+                .arg(Arg::with_name("host").short("b").long("host").help(
+                    "add as host-specific",
+                ))
+                .arg(Arg::with_name("package").short("p").long("package").help(
+                    "package name to install to",
+                )),
+        )
+        .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let dir: PathBuf = match matches.value_of("dir") {
+        Some(path) => {
+            match fs::canonicalize(path) {
+                Ok(path) => path,
+                Err(_) => {
+                    panic!("Invalid 'dir' path");
+                }
+            }
+        }
+        None => env::current_dir().unwrap(),
+    };
+    let target = matches.value_of("target").unwrap_or("");
+
+    let args: Args = Args { dir: PathBuf::from(dir) };
+
+    println!(
+        "Value for dir: {}",
+        fs::canonicalize(args.dir).unwrap().display()
+    );
+
+    if let Some(matches) = matches.subcommand_matches("install") {
+        if matches.is_present("PACKAGE") {
+            for value in matches.values_of("PACKAGE").unwrap() {
+                println!("installing package: {}", value);
+            }
+        }
+    }
+
+    if let Some(matches) = matches.subcommand_matches("remove") {
+        if matches.is_present("PACKAGE") {
+            for value in matches.values_of("PACKAGE").unwrap() {
+                println!("removing package: {}", value);
+            }
+        }
+    }
+
     println!("Hello, world!");
     let mut f: FS = FS::new();
     f.set_mode(Mode::Real);
@@ -75,9 +193,7 @@ impl FS {
         match self.mode {
             Mode::Succeed => true,
             Mode::Fail => false,
-            Mode::Real => {
-                file.as_ref().is_file()
-            }
+            Mode::Real => file.as_ref().is_file(),
         }
     }
 }
